564c56ce2a9065bd1dd7e0d47c7bc08d2243d746
kaigenie <high_java@hotmail.com>
2013-06-23 23:11:25 +0800

    add service support

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -38,6 +38,9 @@
  */
 	CakePlugin::routes();
 
+  // for enable web service
+  Router::parseExtensions();
+
 /**
  * Load the CakePHP default routes. Only remove this if you do not want to use
  * the built-in default routes.
