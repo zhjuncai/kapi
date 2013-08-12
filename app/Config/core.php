cca6457a6da2c280eed74c1bc53b8d500e704433
Ethan Zhang <high_java@hotmail.com>
2013-08-12 23:16:39 +0800

    sync core.php and make consistent with kaigenie

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@ Configure::write('Routing.prefixes', array('admin'));
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9miA');
+	Configure::write('Security.salt', 'Duhjhgds9yus');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683646');
+	Configure::write('Security.cipherSeed', '36789265381');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
